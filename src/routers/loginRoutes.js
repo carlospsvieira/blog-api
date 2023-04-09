9ca88bb3057ab0a806611b262e9ef92203fd22c2
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-09 18:39:22 -0300

    changed login function name to login, before checkForUser

--- src/routers/loginRoutes.js
+++ src/routers/loginRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
-const checkForUser = require('../controllers/loginController');
+const login = require('../controllers/loginController');
 
-router.post('/', checkForUser);
+router.post('/', login);
 
 module.exports = router;
