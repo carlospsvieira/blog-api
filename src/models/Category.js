d09b047fd7b5fa4d6c5130e87cc5bdc13ec735ab
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-10 10:12:50 -0300

    req 07 adds new model for categories

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+
+      name: DataTypes.STRING,
+    },
+    {
+      tableName: "categories",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  return Category;
+};
