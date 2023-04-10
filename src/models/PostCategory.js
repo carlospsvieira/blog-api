897bb55a0a2c84f88858805840cb17bd7b6cfe0d
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-10 11:29:28 -0300

    req 11 creates post categories model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+
+      categoryId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+    },
+    {
+      tableName: "posts_categories",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "posts",
+      through: PostCategory,
+      foreignKey: "categoryId",
+      otherKey: "postId",
+    });
+  };
+
+  return PostCategory;
+};
